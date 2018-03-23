3013f7cc60c00a8991c5024c165b4fc211404b70
JakubGrobelny <300481@uwr.edu.pl>
2018-03-23 01:25:06 +0100

    Changed the way that lists and pairs are displayed

--- writer.py
+++ writer.py
@@ -12,13 +12,20 @@ def present(value):
     #TODO: print lists recursively
     if type(value) == pair:
         ls = [value.get(0)]
+        realLength = 0
         while type(value) == pair:
+            realLength += 1
             value = value.get(1)
             if type(value) == pair:
                 ls.append(value.get(0))
         if value != None:
             ls.append(value)
-        print("'(" + ' '.join(map(str, ls)) + ")")
+        
+        if realLength == 1:
+            print("(" + str(ls[0]) + " . " + str(ls[1]) + ")")
+        else:
+            print("(" + ' '.join(map(str, ls)) + ")")
+    
     elif isinstance(value, types.FunctionType):
         print("#<procedure:" + value.__name__ + ">")
     elif value != "__!@not_a_value@!__":
